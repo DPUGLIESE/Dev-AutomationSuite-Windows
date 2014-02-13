8978358f0b279f1bf4255b5610ea1749db9b0720
Daniel Pugliese <dpugliese@devspark.com>
2014-02-13 10:29:17 -0300

    .gitignore is now working

---
+++ src/test/java/Main/AppServer.java
@@ -0,0 +1,31 @@
+package Main;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.server.ssl.SslSocketConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+import javax.servlet.Servlet;
+import java.io.File;
+
+import org.openqa.selenium.remote.server.DriverServlet;
+
+public class AppServer {
+  private Server server = new Server();
+
+  public AppServer() throws Exception {
+    WebAppContext context = new WebAppContext();
+    context.setContextPath("");
+    context.setWar(".");
+    server.setHandler(context);
+
+    context.addServlet(DriverServlet.class, "/wd/*");
+
+    SelectChannelConnector connector = new SelectChannelConnector();
+    connector.setPort(3001);
+    server.addConnector(connector);
+
+    server.start();
+  }
+}
